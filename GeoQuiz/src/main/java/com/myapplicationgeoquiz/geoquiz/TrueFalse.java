685066ac2f9314354a317ab5b6e7274f0f60b7b2
Jags Babu <Jagbabu@ebay.com>
2014-03-13 14:52:02 +0100

    My First Android Crash App

---
+++ GeoQuiz/src/main/java/com/myapplicationgeoquiz/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.myapplicationgeoquiz.geoquiz;
+
+/**
+ * Created by jagbabu on 3/5/14.
+ */
+public class TrueFalse {
+    private  int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion){
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+
+    }
+
+    public int getmQuestion() {
+        return mQuestion;
+    }
+
+    public void setmQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean ismTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setmTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
